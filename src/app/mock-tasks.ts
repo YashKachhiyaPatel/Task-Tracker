e678f7de806a1cd60cec7ba8ecccad2fe6add576
YashKachhiyaPatel <lifeline242734@gmail.com>
2021-07-09 19:00:04 -0700

    angular project

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,22 @@
+import { Task } from './Task';
+
+export const TASKS: Task[] = [
+    {
+        id: 1,
+        text: 'Doctor Appointment',
+        day: 'May 5th at 2.00pm',
+        reminder: true,
+    },
+    {
+        id: 2,
+        text: 'Meeting',
+        day: 'May 6th at 2.00pm',
+        reminder: true,
+    },
+    {
+        id: 3,
+        text: 'Shopping',
+        day: 'May 3th at 3.00pm',
+        reminder: false,
+    }
+];
